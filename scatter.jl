d362635c7f88e10766ab8d14a1bfae85a91bf608
Richard (Bui Dien Giau) <diengiau@users.noreply.github.com>
2017-04-21 02:56:45 +0800

    Create scatter.jl

---
+++ scatter.jl
@@ -0,0 +1,14 @@
+# load a dataset
+using RDatasets
+iris = dataset("datasets", "iris");
+
+# load the StatPlots recipes (for DataFrames) available via:
+# Pkg.add("StatPlots")
+using StatPlots
+
+# Scatter plot with some custom settings
+scatter(iris, :SepalLength, :SepalWidth, group=:Species,
+        title = "My awesome plot",
+        xlabel = "Length", ylabel = "Width",
+        m=(0.5, [:cross :hex :star7], 12),
+        bg=RGB(.2,.2,.2))
